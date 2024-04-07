8b39ae4ca02010284e45a0cc5276349bd7ad35d7
G Vishok <vishokprasad27@gmail.com>
2024-04-07 21:11:31 +0530

    Initial Commit

---
+++ src/main/java/com/standard/scalerproductservice/ScalerProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.standard.scalerproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/standard/scalerproductservice/ScalerProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standard.scalerproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
